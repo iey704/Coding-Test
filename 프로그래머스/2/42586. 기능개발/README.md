5d976ebffa8afa94f6c7bac7c0bcb5449377e65e
Hyewon Yoon <iey704@gachon.ac.kr>
2024-12-03 13:07:26 +0900

    [level 2] Title: 기능개발, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.25 ms
+메모리: 33.6 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 02일 01:57:01
+2024년 12월 03일 13:07:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,26 +1,24 @@
 function solution(progresses, speeds) {
-  var answer = [];
-  var middle_answer = [];
-
-  for (let i = 0; i < progresses.length; i++) {
-    middle_answer[i] = Math.ceil((100 - progresses[i]) / speeds[i]);
-  }
-
+    var answer = [];
+    var remainDay = [];
     
-  let maxDay = middle_answer[0];
-  let count = 1;
-
-  for (let j = 1; j < middle_answer.length; j++) {
-    if (middle_answer[j] <= maxDay) {
-      count++;
-    } else {
-      answer.push(count);
-      count = 1;
-      maxDay = middle_answer[j];
+    for(let i=0;i<progresses.length;i++){
+        remainDay.push(Math.ceil((100-progresses[i])/speeds[i]));
     }
-  }
+    // console.log(remainDay);
     
-     answer.push(count);
-
-  return answer;
+    let maxDay = remainDay[0];
+    let count = 0;
+    for(let j=0;j<remainDay.length;j++){
+        if(remainDay[j]<=maxDay){
+            count++;
+        }else{
+            answer.push(count);
+            count = 1;
+            maxDay = remainDay[j];
+        }
+    }
+    answer.push(count);
+    
+    return answer;
 }
