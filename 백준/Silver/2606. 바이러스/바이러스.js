25b1b3f8ba2185d4a7e482f0e63d6ad230bad2c4
Hyewon Yoon <iey704@gachon.ac.kr>
2025-07-20 17:09:35 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 9504 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,42 +1,38 @@
-const fs = require('fs');
+const fs = require("fs");
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const num_com = Number(input[0]);
 const num_pair = Number(input[1]);
 
 const graph = [];
-for(let i=0; i<=num_com; i++){
-    graph[i] = [];
+for (let i = 0; i <= num_com; i++) {
+  graph[i] = [];
 }
 
 for (let i = 2; i < 2 + num_pair; i++) {
-    const [n1, n2] = input[i].split(" ").map(Number);
-    graph[n1].push(n2);
-    graph[n2].push(n1);
+  const [n1, n2] = input[i].split(" ").map(Number);
+  graph[n1].push(n2);
+  graph[n2].push(n1);
 }
 
-const visited = Array(num_com+1).fill(false);
+for (let i = 1; i <= num_com; i++) {
+  graph[i].sort((a, b) => a - b);
+}
+
+const visited = Array(num_com + 1).fill(false);
 const result = [];
 
-function bfs(start){
-    const queue = [];
-    queue.push(start);
+function dfs(start) {
     visited[start] = true;
+    result.push(start);
 
-    while(queue.length>0){
-        const node = queue.shift();
-        result.push(node);  
-
-
-        for(let i=0; i<graph[node].length; i++){
-            const neighbor = graph[node][i];
-            if(!visited[neighbor]){
-                visited[neighbor] = true;
-                queue.push(neighbor);
-            }
+    for(let i =0; i<graph[start].length; i++){
+        const node = graph[start][i];
+        if(!visited[node]){
+            dfs(node);
         }
+       
     }
-
 }
 
-bfs(1);
-console.log(result.length-1);
+dfs(1);
+console.log(result.length - 1);
