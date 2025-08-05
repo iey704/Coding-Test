77f6470b9f20843402a69273d0f50064d0a38bc0
Hyewon Yoon <iey704@gachon.ac.kr>
2025-08-06 01:41:02 +0900

    [Silver V] Title: 나이순 정렬, Time: 360 ms, Memory: 48956 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -6,11 +6,16 @@ const N = Number(input[0]);
 const array = [];
 for(let i=1; i<=N; i++){
     const [age, name] = input[i].split(" ");
-    array.push({age: Number(age), name: name}); 
+    // 객체보단 2차원 배열: 메모리 최적화
+    array.push([Number(age),name]); 
 }
 
-answer = array.sort((a,b)=>a.age-b.age);
+// 원본 배열 직접 정렬: 나이 기준
+array.sort((a,b)=>a[0]-b[0]);
 
+// 결과도 한 번에 출력
+const result = [];
 for(let i=0; i<N; i++){
-    console.log(answer[i].age, answer[i].name);
+    result.push(`${array[i][0]} ${array[i][1]}`);
 }
+console.log(result.join('\n'));
