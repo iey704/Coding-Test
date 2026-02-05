2f7bc759e31ca7494461e8040fc63d3a06176539
Hyewon Yoon <iey704@gachon.ac.kr>
2026-02-05 15:09:21 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 144 ms, Memory: 10624 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
@@ -2,42 +2,41 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const [n, m] = input[0].split(' ').map(Number);
 
-// 각자 연결된 친구 넣기
-const graph = Array.from({ length: n + 1 }, () => []);
+let relationship = Array.from({ length: n + 1 }, () => []);
 for (let i = 1; i <= m; i++) {
-    const [user, friend] = input[i].split(' ').map(Number);
-    graph[user].push(friend);
-    graph[friend].push(user);
+    const [u, f] = input[i].split(' ').map(Number);
+    relationship[u].push(f);
+    relationship[f].push(u);
 }
 
-let minBacon = Infinity;
+let minDist = Infinity;
 let minPerson = 0;
-for (let j = 1; j < n + 1; j++) {
-    let totalDist = findBacon(j, graph);
 
-    if (totalDist < minBacon) {
-        minBacon = totalDist;
+for (let j = 1; j <= n; j++) {
+    let baconDist = findBacon(j, relationship);
+
+    if (baconDist < minDist) {
+        minDist = baconDist;
         minPerson = j;
     }
 }
-console.log(minPerson);
 
-// 케빈 베이컨의 수가 가장 작은 사람 (if, 여러명일 경우 번호가 가장 작은 사람)
-function findBacon(user, graph) {
+console.log(minPerson)
+
+function findBacon(user, relationship) {
     let queue = [user];
-    let dist = new Array(n + 1).fill(-1); // 거리 저장
-    dist[user] = 0;
-
-    let sumBacon = 0
+    let dist = new Array(n + 1).fill(-1);
+    dist[0] = 0
+    let sum = 0;
     while (queue.length > 0) {
         let current = queue.shift();
-        for (let next of graph[current]) {
+        for (next of relationship[current]) {
             if (dist[next] === -1) {
                 dist[next] = dist[current] + 1;
-                sumBacon += dist[next];
+                sum += dist[next];
                 queue.push(next);
             }
         }
     }
-    return sumBacon;
+    return sum;
 }
