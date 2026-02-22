ad7a0550682e99a7307a990deac2a8eed0e1e5dd
Hyewon Yoon <iey704@gachon.ac.kr>
2026-02-22 13:33:14 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 7896 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr_num = 10001
+count_arr = [0]*arr_num # 값의 범위에 따라 배열 생성
+
+for i in range(n):
+    count_arr[int(input())] += 1 # 해당 값이 얼마나 나오는지 개수 세기
+
+for j in range(arr_num):
+    if count_arr[j] != 0:
+        for k in range(count_arr[j]):
+            print(j)
