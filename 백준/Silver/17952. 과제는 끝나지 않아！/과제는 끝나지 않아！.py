4e6c68c0a7e1e63f447d0bb78bfcf12963a4e133
Hyewon Yoon <iey704@gachon.ac.kr>
2026-03-10 21:22:08 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 1136 ms, Memory: 89728 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
@@ -13,30 +13,20 @@ for minute in range(n):
         # 새로운 과제가 마무리되면, 중단된 과제 계속해서 진행
         score = hw[1]
         remain = hw[2]
-        if len(to_do) == 0 : # 진행중이던 과제가 없을 때
-            remain -= 1
-            to_do.append((score,remain))
+        remain -= 1
+        to_do.append([score,remain])
 
-            if remain == 0:
-                answer += score
-                to_do.pop()
-        else: # 진행중이던 과제가 있을 때
-            remain -= 1
-            to_do.append((score,remain))
-
-            if remain == 0:
-                answer += score
-                to_do.pop()
+        if remain == 0:
+            answer += score
+            to_do.pop()
     else: # 과제가 주어지지 않았을 때, 최근 과제 이어서 진행    
         if len(to_do) == 0:
             continue
         else:
-            score, remain = to_do.pop()
-            remain -= 1
-            to_do.append((score,remain))
+            to_do[-1][1] -= 1
 
-            if remain == 0:
-                answer += score
+            if to_do[-1][1] == 0: # remain
+                answer += to_do[-1][0] # score
                 to_do.pop()
 
 print(answer)
